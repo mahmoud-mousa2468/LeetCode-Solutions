70e0aa9aeeaeb614dad4faff30387224f610a0ad
Mahmoud Mousa <mo.mousa2468@gmail.com>
2026-03-10 17:39:59 +0200

    add Move Zeroes solution using two-pointer technique
    Implement two-pointer technique to move zeroes.

--- Easy/283-move-zeroes.js
+++ Easy/283-move-zeroes.js
@@ -1,1 +1,14 @@
 
+/**
+ * Problem: 283. Move Zeroes
+ * Strategy: Two-Pointer technique (O(n))
+ */
+var moveZeroes = function(nums) {
+    let lastNonZero = 0;
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== 0) {
+            [nums[lastNonZero], nums[i]] = [nums[i], nums[lastNonZero]];
+            lastNonZero++;
+        }
+    }
+};
