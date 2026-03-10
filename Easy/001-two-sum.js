826ab3cfb9128ca86636d03b8c4282cb2aa2227a
Mahmoud Mousa <mo.mousa2468@gmail.com>
2026-03-10 17:34:00 +0200

    Add solution for Two Sum using Hash Map
    Implemented the twoSum function using a hash map to achieve O(n) performance.

--- Easy/001-two-sum.js
+++ Easy/001-two-sum.js
@@ -1,1 +1,20 @@
+/**
+ * Problem: 1. Two Sum
+ * Difficulty: Easy
+ * Link: https://leetcode.com/problems/two-sum/
+ * * Strategy: Using a Hash Map (Object) to store complements for O(n) performance.
+ */
 
+var twoSum = function(nums, target) {
+    const map = new Map();
+    for (let i = 0; i < nums.length; i++) {
+        const complement = target - nums[i];
+        if (map.has(complement)) {
+            return [map.get(complement), i];
+        }
+        map.set(nums[i], i);
+    }
+};
+
+// Time Complexity: O(n)
+// Space Complexity: O(n)
